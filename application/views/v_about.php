d3c890366e8c2a2c19e9e1f8f729ff44ead13fd4
sriindahdesti <sriindahdesti50@gmail.com>
2021-10-02 12:58:31 +0700

    menambah 5 file, file css dan seting base_url()

---
+++ application/views/v_about.php
@@ -0,0 +1,24 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Sri Indah</li>
+        <li>Nama Belakang : Desti</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Jalan Cibareno No 005, Pasir Baru, Sukabumi</li>
+    </ul>
+    <br>
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Sukabumi</li>
+    </ul>
+    <br>
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Bulutangkis</li>
+        <li>Volly</li>
+    </ul>
+</section>
