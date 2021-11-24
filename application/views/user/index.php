4cf6d0f0e1da87bb3cff4c7ea8491e4f3cd6f4b6
sriindahdesti <sriindahdesti50@gmail.com>
2021-11-24 15:04:19 +0700

    topbar, index, gambar

--- application/views/user/index.php
+++ application/views/user/index.php
@@ -8,11 +8,13 @@
     <div class="card mb-3" style="max-width: 540px;">
         <div class="row no-gutters">
             <div class="col-md-4">
-                <img src="<?= base_url('assets/img/profile/') . $user['image']; ?>" class="card-img" alt="...">
+                <img src="<?= base_url('assets/img/profile/') . $user['image']; ?>" class="card-img">
             </div>
+
+
             <div class="col-md-8">
                 <div class="card-body">
-                    <h5 class="card-title"><?= $user['nama'];?></h5>
+                    <h5 class="card-title"><?= $user['nama']; ?></h5>
                     <p class="card-text"><?= $user['email']; ?></p>
                     <p class="card-text"><small class="textmuted">Jadi member sejak: <br><b><?= date('d F Y', $user['tanggal_input']); ?></b></small></p>
                 </div>
